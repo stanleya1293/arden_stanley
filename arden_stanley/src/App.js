3449db4ea7162d54f493c56bf6f29d6292eea445
Ardeniscool1293 <arden.stanley@yahoo.com>
2024-10-25 13:55:42 -0400

    new file:   arden_stanley/public/IMG_3799.jpg 	modified:   arden_stanley/src/App.css 	modified:   arden_stanley/src/App.js

--- arden_stanley/src/App.js
+++ arden_stanley/src/App.js
@@ -8,6 +8,8 @@ function App() {
         <Route path="/">
           <Route index element={<Home/>} />
           <Route path="about" element={<About/>} />
+          <Route path="projects" element={<Projects/>} />
+          <Route path="skills-and-resume" element={<SkillsAndResume/>} />
         </Route>
       </Routes>
     </BrowserRouter>
@@ -19,6 +21,35 @@ function Home() {
     <div className="Home">
       <Name/>
       <Content/>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div className="About">
+      <div className="ImageContainer">
+        <div className="Overlay">
+          <h1 className="ImageText">Hey there! My name is Arden Stanley.  Nice to meet you!</h1>
+        </div>
+        <img src="IMG_3799.jpg" alt="" width="300" height="500" id="SelfPortrait"/> 
+      </div>
+    </div>
+  );
+}
+
+function Projects() {
+  return (
+    <div className="Home">
+      
+    </div>
+  );
+}
+
+function SkillsAndResume() {
+  return (
+    <div className="Home">
+
     </div>
   );
 }
@@ -38,7 +69,7 @@ function Content() {
     <div className="Content">
       <Item title="About" to="/about"></Item>
       <Item title="Projects" to="/projects"></Item>
-      <Item title="Useful Links" to="/links"></Item>
+      <Item title="Skills + Resume" to="/skills-and-resume"></Item>
       <Outlet/>
     </div>
   );
@@ -56,12 +87,6 @@ function Item({title, to}) {
   );
 }
 
-function About() {
-  return (
-    <div className="Home">
-      
-    </div>
-  );
-}
+
 
 export default App;
